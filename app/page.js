fb6b3359543839b671455faa25493dffb1f0f74a
Fidha Ahamed <fidhaahamedpp@gmail.com>
2025-07-08 22:45:11 +0530

    components

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import Navbar from "./Components/Navbar";
 import Hero from "./Pages/Hero";
 import About from "./Pages/About";
 import WhyIEEE from "./Pages/WhyIEEE";
@@ -5,6 +6,7 @@ import WhyIEEE from "./Pages/WhyIEEE";
 export default function Home() {
   return (
     <>
+      <Navbar />
       <Hero />
       <About />
       <WhyIEEE />
