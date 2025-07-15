118afb7e614d1e333cdc721e55099d79dc28aae5
Fidha Ahamed <fidhaahamedpp@gmail.com>
2025-07-15 23:29:32 +0530

    clash display font added

--- app/Pages/Hero.jsx
+++ app/Pages/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <div>
-      <h1 className='text-6xl'>Welcome</h1>
+      <h1 className='text-6xl font-clash font-light'>Welcome</h1>
     </div>
   )
 }
