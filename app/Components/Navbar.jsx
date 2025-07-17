13a99001a5ec044c9217e8dd20a9ef0d93d5120f
DhiaShams <dhiadhilu123@gmail.com>
2025-07-17 07:16:30 +0530

    Set fond

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -18,16 +18,16 @@ const Navbar = () => {
         <button className="border border-white rounded-full px-4 py-1 hover:bg-white hover:text-black transition">
           Home
         </button>
-        <a href="#about" className="hover:text-orange-400">About</a>
-        <a href="#events" className="hover:text-orange-400">Events</a>
-        <a href="#gallery" className="hover:text-orange-400">Gallery</a>
-        <a href="#chapters" className="hover:text-orange-400">Chapters</a>
-        <a href="#contacts" className="hover:text-orange-400">Contacts</a>
+        <a href="#about" className="hover:text-orange-400 font-[ClashDisplay] font-light" style={{ fontFamily: 'ClashDisplay', fontWeight: 300 }}>About</a>
+        <a href="#events" className="hover:text-orange-400 font-[ClashDisplay] font-light" style={{ fontFamily: 'ClashDisplay', fontWeight: 300 }}>Events</a>
+        <a href="#gallery" className="hover:text-orange-400 font-[ClashDisplay] font-light" style={{ fontFamily: 'ClashDisplay', fontWeight: 300 }}>Gallery</a>
+        <a href="#chapters" className="hover:text-orange-400 font-[ClashDisplay] font-light" style={{ fontFamily: 'ClashDisplay', fontWeight: 300 }}>Chapters</a>
+        <a href="#contacts" className="hover:text-orange-400 font-[ClashDisplay] font-light" style={{ fontFamily: 'ClashDisplay', fontWeight: 300 }}>Contacts</a>
       </div>
 
       {/* Join Us Button */}
       <div>
-        <button className="border border-orange-500 text-orange-500 hover:bg-orange-500 hover:text-white transition rounded-full px-5 py-1 font-semibold">
+        <button className="border border-orange-500 text-orange-500 hover:bg-orange-500 hover:text-white transition rounded-full px-5 py-1 font-semibold font-[ClashDisplay]" style={{ fontFamily: 'ClashDisplay', fontWeight: 600 }}>
           Join Us
         </button>
       </div>
